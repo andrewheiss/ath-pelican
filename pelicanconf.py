11cfecd8836e4f5f3ca856bd1b7a96e9586e2672
Andrew Heiss <andrew@andrewheiss.com>
2016-09-13 00:27:40 -0400

    Latest CV

--- pelicanconf.py
+++ pelicanconf.py
@@ -122,7 +122,7 @@ LOAD_CONTENT_CACHE = False if developing_site else True
 # ------------
 # Site items
 # ------------
-MENUITEMS = [('About', '/'), ('CV', '/files/2016-09-05-andrew-heiss-cv.pdf'),
+MENUITEMS = [('About', '/'), ('CV', '/files/2016-09-13-andrew-heiss-cv.pdf'),
              ('Blog', '/blog/'), ('Research', '/research/'),
              ('Notebook', 'https://notebook.andrewheiss.com/'),
              ('Teaching', '/teaching/'),
